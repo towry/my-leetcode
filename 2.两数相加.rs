6d75ce6c600943b17842cf4fe19fbd96d24fb75e
towry <tovvry@gmail.com>
2021-12-17 09:15:05 +0800

    init

---
+++ 2.两数相加.rs
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=rust
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+fn append_num(origin: i32, next: i32) -> i32 {
+    origin * 10 + next
+}
+
+fn num_of_list(list: &Option<Box<ListNode>>) -> i32 {
+    let mut val = list.unwrap().val;
+
+    while let Some(node) = list.unwrap().next {
+        val = append_num(val, node.val);
+    }
+
+    val
+}
+
+fn num_to_vec_in_revert(n: i32) -> Vec<i32> {
+     n.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .rev()
+        .collect()
+}
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let l1n = num_of_list(&l1);
+        let l2n = num_of_list(&l2);
+
+        let result = l1n + l2n;
+        let list = num_to_vec_in_revert(result);
+        
+        let mut root: Option<Box<ListNode>> = Some(Box::new(ListNode::new(list[0])));
+        let mut node: Option<Box<ListNode>> = None;
+        root.next = node;
+
+        for n in list.iter().skip(1) {
+            let new_node = Some(Box::new(ListNode::new(n)));
+            node.next = new_node;
+            node = new_node;
+        }
+
+        root
+    }
+}
+// @lc code=end
+
