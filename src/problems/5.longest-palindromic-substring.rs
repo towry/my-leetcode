a601286bc0bd1b269f7822dc8ebc357aa16624cc
towry <tovvry@gmail.com>
2022-06-21 08:46:44 +0800

    Add new pro

---
+++ src/problems/5.longest-palindromic-substring.rs
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=rust
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.cn/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (36.79%)
+ * Likes:    5360
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn longest_palindrome(s: String) -> String {
+
+    }
+}
+// @lc code=end
+
