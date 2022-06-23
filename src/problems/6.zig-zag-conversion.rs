4fddc1b48617081d97d60add486ea96ecc96fa8a
towry <tovvry@gmail.com>
2022-06-24 01:25:50 +0800

    up

--- src/problems/6.zig-zag-conversion.rs
+++ src/problems/6.zig-zag-conversion.rs
@@ -17,9 +17,9 @@
  * of rows like this: (you may want to display this pattern in a fixed font for
  * better legibility)
  *
- * 1 . 5 .  9  . 13
- * 2 4 6 8 10 12 14
- * 3   7   11
+ * 0 . 4 .  8  . 12
+ * 1 3 5 7 9 11 13
+ * 2   6   10    14
  *
  * P   A   H   N
  * A P L S I I G
@@ -49,10 +49,10 @@
  * Input: s = "PAYPALISHIRING", numRows = 4
  * Output: "PINALSIGYAHRPI"
  * Explanation:
- * 1     7      13 .   .  19
- * 2   6 8 . 12 14  .  18 20
- * 3 5   9 11   15  17    21
- * 4 .  10      16        22
+ * 0     6      12
+ * 1   5 7 . 11 13
+ * 2 4   8 10
+ * 3 .  9
  *
  * P     I    N
  * A   L S  I G
@@ -91,17 +91,25 @@
 // @lc code=start
 impl Solution {
     fn get_column_size(num_rows: usize, len: usize) -> usize {
-        (len - len % num_rows) / 2
+        let mut value = len;
+        let mut count: usize = 0;
+        while value >= num_rows {
+            count += 1;
+            value -= 2;
+        }
+
+        count + 1
     }
 
     pub fn convert(s: String, num_rows: i32) -> String {
         let middle_size: usize = num_rows as usize - 2;
         let col_size = Solution::get_column_size(num_rows as usize, s.len());
         let chars = s.chars().collect::<Vec<_>>();
-
-        let mut current_middle_size = middle_size;
+        let mut result: Vec<char> = Vec::with_capacity(chars.len());
 
         println!("{}", col_size);
+
+        let mut current_middle_size = middle_size;
 
         for r in 0..num_rows as usize {
             let mut col_index: usize = 0;
@@ -111,38 +119,52 @@ impl Solution {
             while col_index < col_size {
                 // col by col
                 let mut char_index = start_value + 2 * col_index;
+
+                if chars.get(char_index).is_none() {
+                    continue;
+                } else {
+                    result.push(*chars.get(char_index).unwrap());
+                }
+
                 println!(
-                    "{}, {}, {}",
-                    chars.get(char_index).unwrap_or(&'-'),
+                    "{},{},{},{}",
+                    col_index,
                     char_index,
-                    col_index
+                    start_value,
+                    chars.get(char_index).unwrap_or(&'-')
                 );
 
                 col_index += 1;
 
                 // num in middle.
-                col_index += current_middle_size;
-                char_index = start_value + 2 * col_index;
+                let mid_col_index = col_index + current_middle_size;
+                char_index = start_value + 2 * mid_col_index;
+                col_index += middle_size;
                 if current_middle_size >= middle_size {
+                    // skip.
                     continue;
                 }
+                if chars.get(char_index).is_some() {
+                    result.push(*chars.get(char_index).unwrap());
+                }
                 println!(
-                    "{}, {}, {}",
-                    chars.get(char_index).unwrap_or(&'-'),
+                    "-- {},{},{},{}, {}",
+                    col_index,
                     char_index,
-                    col_index
+                    start_value,
+                    current_middle_size,
+                    chars.get(char_index).unwrap_or(&'-')
                 );
-                // skip the middle area.
-                col_index += (middle_size - 1);
             }
 
-            current_middle_size -= 1;
-            if current_middle_size < 0 {
+            if current_middle_size <= 0 {
                 current_middle_size = middle_size;
+            } else {
+                current_middle_size -= 1;
             }
         }
 
-        String::from("")
+        result.into_iter().collect::<String>()
     }
 }
 // @lc code=end
